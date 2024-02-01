6616045696340b1a815ef9a31ef401972e768d81
Alex <aamorgun2003@gmail.com>
2024-02-01 15:21:24 -0700

    Add checkWinner function
    Added a checkWinner function to the gameController module.
    Checks if every index in an array is either and 'X' or an 'O'.
    Does not show anything for a draw yet.

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
 const ticTacBoard = (function () {
-  const gameArray = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-  ];
-  const getArray = () => gameArray;
-  const setArray = (value, idx, innerIdx) => (gameArray[idx][innerIdx] = value);
+  const gameArray = ['O', 'O', 'O', null, null, null, null, null, null];
 
-  return { getArray, setArray };
+  const getArray = () => gameArray;
+
+  const setArray = (value, idx) => (gameArray[idx] = value);
+
+  return {
+    getArray,
+    setArray,
+  };
 })();
 
-const players = function (name, marker) {
-  return { name, marker };
+const players = function (name, marker, turn) {
+  return { name, marker, turn };
 };
 
 const gameController = (function () {
-    const playerX = players('X','X');
-    const playerO = players('O','O');
+  const playerX = players("X", "X", true);
+  const playerO = players("O", "O", false);
 
-    
+  function checkWinner() {
+    let curBoard = ticTacBoard.getArray();
+
+    function checkIdx(idx) {
+      if (idx.every((item) => curBoard[item] === "X")) {
+        return true;
+      } else if (idx.every((item) => curBoard[item] === "O")) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    if (
+      checkIdx([0, 1, 2]) ||
+      checkIdx([3, 4, 5]) ||
+      checkIdx([6, 7, 8]) ||
+      checkIdx([0, 3, 6]) ||
+      checkIdx([1, 4, 7]) ||
+      checkIdx([2, 5, 8]) ||
+      checkIdx([0, 4, 8]) ||
+      checkIdx([6, 4, 2]) && 
+    ) {
+      console.log("WIN??");
+      console.log(ticTacBoard.getArray());
+    }
+  }
+
+  return { checkWinner };
 })();
