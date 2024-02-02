2d5230087bb3d9ca7e4218c482264201008d0535
Alex <aamorgun2003@gmail.com>
2024-02-01 19:30:05 -0700

    Add takeTurns function to gameController
    Added this function to take turns between the two players
    in the console.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const ticTacBoard = (function () {
-  const gameArray = ['O', 'O', 'O', null, null, null, null, null, null];
+  const gameArray = [null, null, null, null, null, null, null, null, null];
 
   const getArray = () => gameArray;
 
@@ -18,9 +18,39 @@ const players = function (name, marker, turn) {
 const gameController = (function () {
   const playerX = players("X", "X", true);
   const playerO = players("O", "O", false);
+  
+  function takeTurns() {
+    if (ticTacBoard.getArray().every((item) => item === null)) {
+      ticTacBoard.setArray(
+        playerX.marker,
+        Math.floor(Math.random() * ticTacBoard.getArray().length)
+      );
+
+      playerX.turn = !playerX.turn;
+      playerO.turn = !playerO.turn;
+    } else {
+      if (playerX.turn) {
+        checkWinner();
+        ticTacBoard.setArray(
+          playerX.marker,
+          Math.floor(Math.random() * ticTacBoard.getArray().length)
+        );
+        playerX.turn = !playerX.turn;
+        playerO.turn = !playerO.turn;
+      } else {
+        checkWinner();
+        ticTacBoard.setArray(
+          playerO.marker,
+          Math.floor(Math.random() * ticTacBoard.getArray().length)
+        );
+        playerO.turn = !playerO.turn;
+        playerX.turn = !playerX.turn;
+      }
+    }
+  }
 
   function checkWinner() {
-    let curBoard = ticTacBoard.getArray();
+    const curBoard = ticTacBoard.getArray();
 
     function checkIdx(idx) {
       if (idx.every((item) => curBoard[item] === "X")) {
@@ -40,12 +70,19 @@ const gameController = (function () {
       checkIdx([1, 4, 7]) ||
       checkIdx([2, 5, 8]) ||
       checkIdx([0, 4, 8]) ||
-      checkIdx([6, 4, 2]) && 
+      checkIdx([2, 4, 6])
     ) {
       console.log("WIN??");
       console.log(ticTacBoard.getArray());
+    } else if (
+      curBoard.every((cell) => cell === "X" || (cell === "O" && cell !== null))
+    ) {
+      console.log("draw??");
+      console.log(ticTacBoard.getArray());
+    } else {
+      console.log("playing");
     }
   }
 
-  return { checkWinner };
+  return { checkWinner, takeTurns };
 })();
